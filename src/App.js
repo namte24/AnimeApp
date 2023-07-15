7ccf0106551e032dab1c4c1212705c1e4ff92fe9
tenzing <tenam2000@hotmail.com>
2023-07-15 21:19:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AnimeItem from "./components/AnimeItem";
+import Gallery from "./components/Gallery";
+import Homepage from "./components/Home";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/anime/:id" element={<AnimeItem />} />
+        <Route path="/character/:id" element={<Gallery />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
